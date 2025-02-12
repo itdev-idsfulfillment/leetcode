e9dc18e1c2bf1deb31900df5ee8b18cd6f684ac1
Steven Lyon <lyon.steven@gmail.com>
2025-02-12 09:21:31 -0700

    Adding first leetcode example
    - Thank you, Rider

---
+++ RemoveOccurencesOfASubstring/Tests/RemoveOccurencesOfASubstringTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace RemoveOccurencesOfASubstring.Tests;
+
+public class RemoveOccurencesOfASubstringTests
+{
+    [Theory]
+    [InlineData("daabcbaabcbc", "abc", "dab")]
+    [InlineData("axxxxyyyyb", "xy", "ab")]
+    [InlineData("aaaaaaaaa", "a", "")]
+    public void RemoveOccurrences_IsExpected(string? s, string? part, string? expectedValue)
+    {
+        var result = new Solution().RemoveOccurrences(s, part);
+        Assert.True(result == expectedValue);
+    }
+
+    [Theory]
+    [InlineData("abc", null, "abc")]
+    [InlineData("abc", "", "abc")]
+    [InlineData("abc", "xy", "abc")]
+    public void RemoveOcurrences_ShouldNotRemove(string? s, string? part, string? expectedValue)
+    {
+        var result = new Solution().RemoveOccurrences(s, part);
+        Assert.True(result == expectedValue);
+    }
+
+    [Theory]
+    [InlineData(null, null, null)]
+    [InlineData(null, "abc", null)]
+    [InlineData("abc", null, "abc")]
+    public void RemoveOccurrences_Nulls(string? s, string? part, string? expectedValue)
+    {
+        var result = new Solution().RemoveOccurrences(s, part);
+        Assert.True(result == expectedValue);
+    }
+}
