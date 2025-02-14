3d70a01157f2f21c5eb6ac314ab8f4dc341a374b
Steven Lyon <lyon.steven@gmail.com>
2025-02-14 09:29:49 -0700

    Visual Studio needs an xunit runner package

--- RemoveTrailingZerosFromAString/Tests/RemoveTrailingZerosFromAString.cs
+++ RemoveTrailingZerosFromAString/Tests/RemoveTrailingZerosFromAString.cs
@@ -5,9 +5,9 @@ namespace RemoveTrailingZerosFromAString.Tests;
 public class RemoveTrailingZerosFromAString
 {
     [Theory]
-    [InlineData("12300", "123")]
     [InlineData("00000", "")]
-    [InlineData("123", "123")]
+    [InlineData("12345", "12345")]
+    [InlineData("123000000000000000000000000000000000000000", "123")]
     public void RemoveOccurrences_IsExpected(string? s, string? expectedValue)
     {
         var result = new Solution().RemoveTrailingZeros(s);
